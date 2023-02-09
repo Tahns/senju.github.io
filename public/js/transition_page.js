a28bf5b4674f516d17786a987409c3e8a64a9813
akumq <69771735+akumq@users.noreply.github.com>
2023-02-09 22:33:27 +0100

    Add files via upload

---
+++ public/js/transition_page.js
@@ -0,0 +1,154 @@
+content_page1 = [
+    `<h2><strong>Culte Osis</strong></h2><img src="https://i.pinimg.com/236x/dd/b5/03/ddb503632f448aa53f629a22068bbacf.jpg" style="width: auto;height: 60%;" alt="">`,
+    `
+        <h2> 1978 - Création du Culte</h2>
+        <p>
+        L'année 1978 fut marquée par la fondation du Culte d'OSIS. Ce culte, dirigé par une poignée de familles, fut créé dans le but de réunir les Ghouls et de les protéger des persécutions humaines. La famille Tsukyama, dont je suis issu, était parmi les fondateurs de ce Culte.
+        <br><br>
+        À ses débuts, le Culte d'OSIS n'était qu'un petit regroupement de Ghouls en quête de sécurité et de reconnaissance. Cependant, les fondateurs, y compris ma famille, avaient en tête une vision plus grande, une vision d'un futur où les Ghouls auraient une place digne dans la société. Ils croyaient en l'unité et en la coopération pour atteindre ce but.
+        <br><br>
+        En cette année 1978, le Culte d'OSIS était encore à ses balbutiements, mais déjà il portait en lui les germes d'un avenir prometteur pour les Ghouls. Et même si les défis étaient nombreux, les fondateurs, y compris ma famille, étaient déterminés à les surmonter pour bâtir un monde meilleur pour notre peuple.
+        </p>
+
+        <button onclick="PageSuivante(1)">Page Précédente</button></div>
+    `,
+    `
+        <h2> 2000 - l'essor du Culte </h2>
+
+        <p>
+        L'an 2000 fut marqué par un tournant majeur pour le Culte d'OSIS. Les premières églises virent le jour, offrant une plateforme pour les membres du culte pour partager leurs croyances et leur foi. Cette année vit également la naissance d'un nouveau mouvement au sein du culte, appelé les pacifistes. Ceux-ci prônaient une approche plus douce envers les humains et voulaient éviter tout acte violent.
+        <br><br>
+        Cependant, cela ne plaisait pas à tout le monde, et la division entre les pacifistes et les autres membres du culte s'accentua de plus en plus. Les fondateurs de la famille Tsukyama étaient particulièrement réticents à cette nouvelle vision, considérant que la violence était nécessaire pour protéger les ghouls et les maintenir en sécurité.
+        <br><br>
+        Malgré ces divergences, l'église pacifiste continua de grandir et d'attirer de plus en plus de membres. Les fondateurs de la famille Tsukyama commençaient à s'inquiéter de la tournure que prenait le culte, craignant que leur position de pouvoir ne soit menacée par ce nouveau mouvement.
+ </p>
+    
+        <button onclick="PageSuivante(2)">Page Précédente</button></div>
+    `,
+    `
+        <p>
+        Les premières églises du Culte d'OSIS virent le jour en 2000, mais ce fut en 2001 que la scission entre les factions pacifiques et violentes devint plus évidente. Bien que nous ayons travaillé ensemble pendant un certain temps, les tensions entre les deux groupes ne tardèrent pas à monter.
+        <br><br>
+        L'année 2001 fut donc marquée par la naissance de deux visions distinctes pour notre avenir, qui allaient déterminer le destin du Culte d'OSIS pour les années à venir.
+        
+        </p>
+        <h2> 2005 - Croissance </h2>
+
+        <p>
+        L'année 2005 fut une période de croissance considérable pour la famille Tsukyama, alors que les factions pacifiques du Culte D'Osis peinaient à maintenir leur influence et leur position dans l'équilibre de pouvoir. La famille Tsukyama, quant à elle, s'était forgée une réputation de force et de détermination dans la poursuite de leurs objectifs. C'était également à ce moment que le leadership de la famille commençait à prendre forme, avec Shigeru Tsukyama, l'un des frères, émergeant comme un futur dirigeant potentiel.
+        <br><br>
+        </p>
+
+        <button onclick="PageSuivante(3)">Page Précédente</button></div>
+    `,
+    `
+    <h2> 2005-2017 </h2>
+
+    <p>
+        Pendant cette période rien ne ce passas les Tsukyama ce dévellopé tandis que le culte gagné en apotre, on peut quand même dire que dans ce laps de temps des ghouls sont sortie de l'ombre, les frères Tsukyama 
+        <ul>
+            <li>Shigeru Tsukyama - Rabbit </li>
+            <li>Sayenori Tsukyama - Sans Couronne </li>
+            <li>Yuu Tsukyama - Kirito </li>
+        </ul>
+        Ensemble ses ghouls était un trio de choc craignez par le CCG mais aussi par les ghoules
+    </p>
+
+    <h2> 2017 - Prise de Pouvoir </h2> 
+
+    <p>
+
+    En l'année 2017, alors que la famille Tsukyama continuait de s'agrandir et de prendre de l'influence au sein du Culte d'OSIS, ils décidèrent de prendre une nouvelle direction en créant un groupe nommé "Rosenwald". Ce dernier se spécialisait dans la gestion d'un restaurant chic qui organisait des dégustations pour les Ghouls nobles. Cet endroit prestigieux permettait à la famille Tsukyama de se faire connaître auprès de la haute société Ghoule et d'augmenter leur influence politique. Les années passèrent et les membres de Rosenwald étaient considérés comme des experts en gastronomie Ghoule et étaient très appréciés par les membres les plus influents du Culte d'OSIS.
+    <br>
+    </p>
+    <button onclick="PageSuivante(4)">Page Précédente</button></div>
+    `,
+    `
+    <p>
+    Cette année fut également marquée par une remise en question de la direction du Culte. Les membres du culte commencèrent à se poser des questions sur les objectifs de leur organisation et sur la façon dont ils les atteignaient. Certaines factions pacifiques commencèrent à se faire entendre, ce qui amena Hideaki et ses frères à agir pour éliminer ces factions et prendre le contrôle total du Culte D'osis.
+    <br><br>
+    C'est à cette période que le groupe Costume Blanc fut créé, un groupe violent et influent au sein du Culte D'osis. Leur objectif était de faire régner la terreur parmi les humains et de montrer que les ghouls étaient supérieurs à leurs oppressors. Les actions du Costume Blanc firent parler d'eux à travers tout le Japon, et leur nom devint synonyme de violence et de terreur.
+    </p>
+
+    <button onclick="PageSuivante(5)">Page Précédente</button></div>
+    `
+];
+content_page2 = [
+    `
+        <h2> Introduction </h2>
+       <p> 
+        Le Culte d'OSIS est un groupe religieux ayant été fondé en 1978 par plusieurs familles, dont les Tsukyama.  <br> <br> Ce culte était à l'origine divisé en deux factions: une qui prônait une approche pacifique avec les humains, et une autre qui était plus violente. Au fil des années, la faction violente a pris de l'ascendant et a éliminé les membres de la faction pacifique. En 2017, la famille Tsukyama a créé un groupe appelé Rosenwald qui gérait un restaurant chic pour les ghouls nobles. En 2020, les Rosenwald ont été dissous et la famille Tsukyama a pris le contrôle du Culte d'OSIS en éliminant toutes les factions pacifiques. En 2021, ils ont créé un groupe au sein du culte appelé Costume Blanc, qui est devenu le groupe le plus violent et influent au sein du culte. La situation actuelle du culte d'OSIS reste incertaine.
+        </p>
+        <button onclick="PageSuivante(2)">Page Suivante</button></div>
+    `,
+
+    `
+        <h2> 1995 - Apparition des Pacifique</h2>
+    <p>
+    En l'année 1995, les vents du changement soufflaient fort au sein du Culte d'OSIS. Les murmures de certains membres étaient de plus en plus présents, appelant à un revirement de la doctrine de la violence envers les humains. Ils souhaitaient établir une méthode plus pacifique pour coexister avec eux, sans pour autant causer de dommages inutiles.
+
+    <br> <br>
+    Cependant, leur vision était considérée comme une hérésie par les autres membres plus radicaux, qui considéraient la haine envers les humains comme la seule voie à suivre. Une lutte de pouvoir s'engagea alors entre les deux factions, pour déterminer la direction à donner au Culte d'OSIS pour l'avenir.
+
+    <br> <br>
+
+    Les frères Tsukyama, en tant que membre de la Famille Tsukyama, était témoin de cette lutte interne. Il observait les événements avec une certaine réserve, car il savait que les choix effectués à cette époque allaient déterminer le sort du Culte pour les années à venir. Il était conscient que leur avenir en dépendait, et il était déterminé à ce que le Culte d'OSIS soit dirigé sur la voie de la force et de la puissance, pour que les ghouls puissent vivre dans un monde où ils ne seraient pas persécutés par les humains.
+    <button onclick="PageSuivante(3)">Page Suivante</button></div>
+    </p>
+    `,
+    `
+    <p>
+    Néanmoins, ils décidèrent de rester en retrait, préférant observer les événements pour voir comment les choses allaient évoluer avant de prendre une décision définitive. Les années à venir allaient déterminer l'avenir du culte et le sort des membres fondateurs de la famille Tsukyama.
+    </p>
+
+    <h2> 2001 - Début des Révoltes </h2>
+
+    <p>
+    2001 marqua un tournant décisif pour le Culte d'OSIS. Cette année vit naître deux factions distinctes au sein de notre organisation, qui défendaient des visions différentes de notre place dans la société.
+    
+    <br><br>
+    D'un côté, il y avait ceux qui prônaient la violence, la haine envers les humains et la domination de notre race sur la leur. De l'autre, il y avait ceux qui cherchaient à coexister pacifiquement avec les humains, à travailler en secret pour éviter toute répression.
+    
+    <br><br>
+    Je suis fier de dire que ma famille et moi-même étions à la tête de la première faction, nous croyions fermement que notre devoir était de protéger notre peuple et de le mener vers un avenir prospère, même si cela signifiait éliminer ceux qui se dressaient sur notre chemin.
+    </p>
+
+    <button onclick="PageSuivante(4)">Page Suivante</button></div>
+    `,
+    `
+    <p> 
+    Le culte, dans son ensemble, était en train de grandir de manière rapide, mais il y avait une certaine incertitude quant à l'avenir du mouvement. Les factions pacifiques s'inquiétaient du développement de plus en plus agressif de la famille Tsukyama, alors que cette dernière prônait une approche plus déterminée dans leurs relations avec les humains. Cependant, malgré ces tensions, le Culte D'osis continuait de prospérer et de s'étendre, avec de plus en plus de Ghouls se joignant à ses rangs pour trouver refuge et soutien dans ce qui était devenu un véritable mouvement communautaire.
+    <br><br>
+    Au cours de l'année 2005, la famille Tsukyama a également commencé à explorer de nouvelles opportunités d'affaires et de commerce, cherchant à renforcer leur position financière et économique. Ils ont commencé à investir dans des entreprises et des commerces liés à la restauration, en faisant la promotion de leurs produits auprès des Ghouls les plus influents de la société. Ce fut une période déterminante pour la famille, qui allait continuer de croître et de se développer pour devenir l'une des forces les plus influentes du Culte D'osis.
+
+    </p>
+
+    <button onclick="PageSuivante(5)">Page Suivante</button></div>
+    `,
+    `
+    <p> 
+    Toutefois, cet élan de succès ne dura pas éternellement. En l'année 2020, les activités de Rosenwald furent dissoutes et les héritiers de la famille Tsukyama firent leur apparition publique dans une vidéo qui circula rapidement dans les réseaux Ghoules. Ils démontraient ainsi leur force et leur influence au sein du Culte d'OSIS, et prouvaient qu'ils étaient prêts à tout pour faire valoir leur pouvoir. Les membres de Rosenwald n'étaient plus considérés comme des experts gastronomiques, mais comme des leaders politiques influents au sein du Culte.
+    <br><br>
+    </p>
+
+    <h2> 2020 - Dernière pétale</h2>
+
+    <p>
+    L'année 2020 fut marquée par une grande dissension au sein du Culte D'osis. La famille Tsukyama avait créé le groupe Rosenwald pour gérer un restaurant chic où ils organisaient des dégustations pour les ghouls nobles. Cependant, cet établissement fut dissous, et les héritiers de la famille Tsukyama se montrèrent dans une vidéo publique.
+    <br><br>
+    </p>
+
+    
+    <button onclick="PageSuivante(6)">Page Suivante</button></div>
+    `,
+    ``
+]
+
+function PageSuivante(nb) {
+    document.getElementById("page1").innerHTML = content_page1[nb - 1];
+    document.getElementById("page2").innerHTML = content_page2[nb - 1];
+};
+
+setTimeout(function() {
+    PageSuivante(1);
+}, 10);
