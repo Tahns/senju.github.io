6058a194a180e07cb6488fa225d8e58519208adc
Tahns <161220499+Tahns@users.noreply.github.com>
2024-02-25 18:21:44 +0100

    Update transition_page.js

--- public/js/transition_page.js
+++ public/js/transition_page.js
@@ -47,33 +47,11 @@ content_page1 = [
         <button onclick="PageSuivante(3)">Page Précédente</button></div>
     `,
     `
-    <h2> 2005-2017 </h2>
-
-    <p>
-        Pendant cette période rien ne ce passas les Tsukyama ce dévellopé tandis que le culte gagné en apotre, on peut quand même dire que dans ce laps de temps des ghouls sont sortie de l'ombre, les frères Tsukyama 
-        <ul>
-            <li>Shigeru Tsukyama - Rabbit </li>
-            <li>Sayenori Tsukyama - Sans Couronne </li>
-            <li>Yuu Tsukyama - Kirito </li>
-        </ul>
-        Ensemble ses ghouls était un trio de choc craignez par le CCG mais aussi par les ghoules
-    </p>
-
-    <h2> 2017 - Prise de Pouvoir </h2> 
-
-    <p>
-
-    En l'année 2017, alors que la famille Tsukyama continuait de s'agrandir et de prendre de l'influence au sein du Culte d'OSIS, ils décidèrent de prendre une nouvelle direction en créant un groupe nommé "Rosenwald". Ce dernier se spécialisait dans la gestion d'un restaurant chic qui organisait des dégustations pour les Ghouls nobles. Cet endroit prestigieux permettait à la famille Tsukyama de se faire connaître auprès de la haute société Ghoule et d'augmenter leur influence politique. Les années passèrent et les membres de Rosenwald étaient considérés comme des experts en gastronomie Ghoule et étaient très appréciés par les membres les plus influents du Culte d'OSIS.
-    <br>
-    </p>
     <button onclick="PageSuivante(4)">Page Précédente</button></div>
     `,
     `
     <p>
-    Cette année fut également marquée par une remise en question de la direction du Culte. Les membres du culte commencèrent à se poser des questions sur les objectifs de leur organisation et sur la façon dont ils les atteignaient. Certaines factions pacifiques commencèrent à se faire entendre, ce qui amena Hideaki et ses frères à agir pour éliminer ces factions et prendre le contrôle total du Culte D'osis.
-    <br><br>
-    C'est à cette période que le groupe Costume Blanc fut créé, un groupe violent et influent au sein du Culte D'osis. Leur objectif était de faire régner la terreur parmi les humains et de montrer que les ghouls étaient supérieurs à leurs oppressors. Les actions du Costume Blanc firent parler d'eux à travers tout le Japon, et leur nom devint synonyme de violence et de terreur.
-    </p>
+
 
     <button onclick="PageSuivante(5)">Page Précédente</button></div>
     `
@@ -143,25 +121,17 @@ content_page2 = [
     <p>
      <h2> Nindo </h2>
      <br><br>
+    <br><br>
     "Je vise à instauré l'ordre dans mon village, inspiré des livres des temps anciens de Keitaro et Karasuma Senju, par la suite perpétuer sa réputation d'un village sérieux et de danger pour préserver la paix. De plus, j'aspire à devenir un grand maître du Taijutsu et Kenjutsu."
     <br><br>
-    Au cours de l'année 2005, la famille Tsukyama a également commencé à explorer de nouvelles opportunités d'affaires et de commerce, cherchant à renforcer leur position financière et économique. Ils ont commencé à investir dans des entreprises et des commerces liés à la restauration, en faisant la promotion de leurs produits auprès des Ghouls les plus influents de la société. Ce fut une période déterminante pour la famille, qui allait continuer de croître et de se développer pour devenir l'une des forces les plus influentes du Culte D'osis.
 
     </p>
-
-    <button onclick="PageSuivante(5)">Page Suivante</button></div>
     `,
     `
     <p> 
-    Toutefois, cet élan de succès ne dura pas éternellement. En l'année 2020, les activités de Rosenwald furent dissoutes et les héritiers de la famille Tsukyama firent leur apparition publique dans une vidéo qui circula rapidement dans les réseaux Ghoules. Ils démontraient ainsi leur force et leur influence au sein du Culte d'OSIS, et prouvaient qu'ils étaient prêts à tout pour faire valoir leur pouvoir. Les membres de Rosenwald n'étaient plus considérés comme des experts gastronomiques, mais comme des leaders politiques influents au sein du Culte.
-    <br><br>
     </p>
 
-    <h2> 2020 - Dernière pétale</h2>
-
-    <p>
-    L'année 2020 fut marquée par une grande dissension au sein du Culte D'osis. La famille Tsukyama avait créé le groupe Rosenwald pour gérer un restaurant chic où ils organisaient des dégustations pour les ghouls nobles. Cependant, cet établissement fut dissous, et les héritiers de la famille Tsukyama se montrèrent dans une vidéo publique.
-    <br><br>
+    
     </p>
 
     
