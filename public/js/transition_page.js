23c24cf0c2e1859d597c114bcb3026094bd3f2cf
Tahns <161220499+Tahns@users.noreply.github.com>
2024-02-25 16:51:32 +0100

    Update transition_page.js

--- public/js/transition_page.js
+++ public/js/transition_page.js
@@ -1,9 +1,20 @@
 content_page1 = [
     `<h2><strong>Fiche Personnage</strong></h2><img src="https://ideogram.ai/api/images/direct/a4XlYue-Q-u7AoyfpIRHjQ.jpg" style="width: auto;height: 80%;" alt="">`,
     `
-        <h2> 1978 - Création du Culte</h2>
+        <h2> Ambitions </h2>
         <p>
-        L'année 1978 fut marquée par la fondation du Culte d'OSIS. Ce culte, dirigé par une poignée de familles, fut créé dans le but de réunir les Ghouls et de les protéger des persécutions humaines. La famille Tsukyama, dont je suis issu, était parmi les fondateurs de ce Culte.
+        Chef Section Policière
+Dérogation section Militaire
+Maitre Taijutsu , Kenjutsu (sans portes ou si possible la première seulement afin ne pas être overcheat, je ne recherche pas cela mais l'exploitation des techniques de base actuelles) 
+Crée une meilleure organisation des Sections , créer des liens avec les tetes du Village et partager les bonnes idées communes.
+Mettre en place un systeme de responssabilités pour chaques hauts gradés
+Faire en sorte que les rapports ont une réel utilité et ne pas écrire pour écrire comme crée des missions pour tout les ninjas par grade et ceux ayant rendu un rapport avec tout de réalisé seront récompensé.
+Mettre en place un systeme d'entrainement régulier pour les combats en groupe , il y a un trop grand écart entre les 2 Villages.
+Crée un réel systeme économique pas forcement utilisé uniquement les taxes , comme un systeme de rachats des ressources et de revente pour les habitants de Konoha pour les armures des hauts gradés.
+
+Ne pas laisser les jeunes ninjas se relâcher et proposer un maximum d'activités utiles pour tous.
+
+Surtout un personnage sérieux , strict et leader au devant de tous avec un développement progressif par grade).
         <br><br>
         À ses débuts, le Culte d'OSIS n'était qu'un petit regroupement de Ghouls en quête de sécurité et de reconnaissance. Cependant, les fondateurs, y compris ma famille, avaient en tête une vision plus grande, une vision d'un futur où les Ghouls auraient une place digne dans la société. Ils croyaient en l'unité et en la coopération pour atteindre ce but.
         <br><br>
